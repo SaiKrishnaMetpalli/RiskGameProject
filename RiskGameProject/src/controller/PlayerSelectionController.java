120744eefbb41cb4e859211ce2f03c1e8789e321
hp pc <hp pc@DESKTOP-TCG23FP.wireless.concordia.ca>
2019-10-03 13:22:04 -0400

    Player adding and remove operations created

--- RiskGameProject/src/controller/PlayerSelectionController.java
+++ RiskGameProject/src/controller/PlayerSelectionController.java
@@ -1,5 +1,36 @@
 package controller;
 
-public class PlayerSelectionController {
+import java.util.ArrayList;
 
+public class PlayerSelectionController {
+	public void playerAddRemove()
+	{
+		int choice = 0;			
+		ArrayList<String> al1 = new ArrayList<String>();
+		while(choice != 5)
+		{			
+			switch(choice) {
+			case 1: // add player name
+				System.out.println("Enter the player name");
+				//String playerName = sc.nextLine();
+				//al1.add(playerName);
+				break;
+				
+			case 2: // remove player name
+				System.out.println("Enter the player name");
+				//String playerNameRem = sc.nextLine();
+				//al1.remove(playerNameRem);
+				break;
+			case 5: // exit
+				System.exit(0);
+				break;
+			
+			default:
+				System.out.println("\n==================================");
+				System.out.println("\n\t Error! Please Enter Your Choice(1 to 5)");				
+				break;
+			}
+			
+		}
+	}
 }
