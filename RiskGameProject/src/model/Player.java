2ebafdcfe30b0c1b8bd37c17677a134c75bd2a54
ashishchaudhary123 <ashishchaudhary123@users.noreply.github.com>
2019-10-30 21:24:03 -0400

    Attack controller implementation Attacker and defender information added

--- RiskGameProject/src/model/Player.java
+++ RiskGameProject/src/model/Player.java
@@ -12,7 +12,12 @@ public class Player {
 	private ArrayList<Integer> ownedArmiesList;
 	private HashMap<String, Integer> ownedCountriesArmiesList;
 	private String currentPlayerTurn;
-	
+	private String attackerName;
+	private String defenderName;
+	private String attackerCountry;
+	private String defenderCountry;
+	private ArrayList<Integer> attackerDice;
+	private ArrayList<Integer> defenderDice;
 	/**
 	 * @Default Constructor This method initiates the variables
 	 */
@@ -54,4 +59,54 @@ public class Player {
 	public void setCurrentPlayerTurn(String currentPlayerTurn) {
 		this.currentPlayerTurn = currentPlayerTurn;
 	}
+
+	public String getAttackerName() {
+		return attackerName;
+	}
+
+	public void setAttackerName(String attackerName) {
+		this.attackerName = attackerName;
+	}
+
+	public String getDefenderName() {
+		return defenderName;
+	}
+
+	public void setDefenderName(String defenderName) {
+		this.defenderName = defenderName;
+	}
+
+	public String getAttackerCountry() {
+		return attackerCountry;
+	}
+
+	public void setAttackerCountry(String attackerCountry) {
+		this.attackerCountry = attackerCountry;
+	}
+
+	public String getDefenderCountry() {
+		return defenderCountry;
+	}
+
+	public void setDefenderCountry(String defenderCountry) {
+		this.defenderCountry = defenderCountry;
+	}
+
+	public ArrayList<Integer> getAttackerDice() {
+		return attackerDice;
+	}
+
+	public void setAttackerDice(ArrayList<Integer> attackerDice) {
+		this.attackerDice = attackerDice;
+	}
+
+	public ArrayList<Integer> getDefenderDice() {
+		return defenderDice;
+	}
+
+	public void setDefenderDice(ArrayList<Integer> defenderDice) {
+		this.defenderDice = defenderDice;
+	}
+	
+	
 }
