67496fabf50ccb2d20005736760f89a0f36ea591
garima <garimadawar28@gmail.com>
2019-11-26 19:44:55 -0500

    Added javadoc for strategy interface

--- RiskGameProject/src/controller/Strategy.java
+++ RiskGameProject/src/controller/Strategy.java
@@ -3,9 +3,17 @@ package controller;
 import model.GameMap;
 import model.Player;
 import model.PlayersList;
-
+/**
+ * This is the strategy interface which contains execute method
+ */
 public interface Strategy {
-
+    /**
+     * this method is used to execute strategy
+     * @param gm this contains the game map
+     * @param pl this contains the list of players
+     * @param player this contains player data
+     * @return String 
+     */
 	String executeStrategy(GameMap gm , PlayersList pl , Player player);
 		
 }
