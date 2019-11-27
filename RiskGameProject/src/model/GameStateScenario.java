a3f4de89f8db3415ab2d4c4192a5aa86fad0fd18
Gagan Jaswal <gagandeepjaswal227@gmail.com>
2019-11-26 23:34:46 -0500

    Updated java doc for game state scenario model

--- RiskGameProject/src/model/GameStateScenario.java
+++ RiskGameProject/src/model/GameStateScenario.java
@@ -6,18 +6,27 @@ package model;
 public class GameStateScenario extends GameStateBuilder {
 	/**
 	 * This function build the game map
+	 * @param gm  it contains the game map
+	 * @author Sai Krishna
 	 */
 	public void buildGameMap(GameMap gm) {
 		gameState.setGameMap(gm);
 	}
 	
 	/**
-	 * 
+	 * This function contains the player list
+	 * @param pl   it contains the player list
+	 * @author Gagan Jaswal
 	 */
 	public void buildPlayersList(PlayersList pl) {
 		gameState.setPlayersList(pl);
 	}
 	
+	/**
+	 * This function contains the player
+	 * @param p  it contains the player
+	 * @author garimadawar
+	 */
 	public void buildPlayer(Player p) {
 		gameState.setPlayer(p);
 	}
