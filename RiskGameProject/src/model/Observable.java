6c470f54f96a68d40a5b33c222a636b3983e0c18
Gagan Jaswal <gagandeepjaswal227@gmail.com>
2019-11-26 23:37:25 -0500

    Updated java doc for observable model

--- RiskGameProject/src/model/Observable.java
+++ RiskGameProject/src/model/Observable.java
@@ -6,12 +6,17 @@ import java.util.List;
 import view.Observer;
 
 public class Observable {
+	
+	/**
+	 * This private variable contains the list of observers
+	 */
 	private List<Observer> observers = new ArrayList<Observer>();
 
 	/**
 	 * This method is used for attaching the observer to this class
 	 * 
 	 * @param o this variable contains Observer object
+	 * @author Sai Krishna
 	 */
 	public void attach(Observer o) {
 		this.observers.add(o);
@@ -21,6 +26,7 @@ public class Observable {
 	 * This method is used for detaching the observer to this class
 	 * 
 	 * @param o this variable contains Observer object
+	 * @author Sai Krishna
 	 */
 	public void detach(Observer o) {
 		if (!observers.isEmpty()) {
@@ -32,6 +38,7 @@ public class Observable {
 	 * This method notifies the observer that state has changed
 	 * 
 	 * @param observable this variable contains the observable object state
+	 * @author Ashish
 	 */
 	public void notifyObservers(Observable observable, Object x) {
 		for (Observer observer : observers) {
