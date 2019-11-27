44aea42f685ea2b9caaefd1d1d1f252146d5209b
Gagan Jaswal <gagandeepjaswal227@gmail.com>
2019-11-26 23:02:35 -0500

    Java doc for game state builder model

--- RiskGameProject/src/model/GameStateBuilder.java
+++ RiskGameProject/src/model/GameStateBuilder.java
@@ -1,17 +1,45 @@
 package model;
 
+/**
+ * This class contains the game state builder attributes
+ */
 public abstract class GameStateBuilder {
+	/**
+	 * This variable contains game state
+	 */
 	protected GameState gameState;
-
+	
+	/**
+	 * This function get the game state
+	 * @return This returns the game state
+	 */
 	public GameState getGameState() {
 		return gameState;
 	}
-
+	
+	/**
+	 * This function set the game state
+	 * @param gameState
+	 */
 	public void setGameState(GameState gameState) {
 		this.gameState = gameState;
 	}
 	
+	/**
+	 * This is an abstract method for setting the game map
+	 * @param gm
+	 */
 	public abstract void buildGameMap(GameMap gm);	
-	public abstract void buildPlayersList(PlayersList pl);	
+	
+	/**
+	 * This is an abstract method for player list
+	 * @param pl
+	 */
+	public abstract void buildPlayersList(PlayersList pl);
+	
+	/**
+	 * This is an abstract method for player
+	 * @param p
+	 */
 	public abstract void buildPlayer(Player p);	
 }
