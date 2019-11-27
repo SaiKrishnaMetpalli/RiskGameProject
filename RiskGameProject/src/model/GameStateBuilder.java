f155911a46b4d6af63efb467cd3e7371089a6c31
Gagan Jaswal <gagandeepjaswal227@gmail.com>
2019-11-26 23:06:15 -0500

    Updated java doc for game state builder model

--- RiskGameProject/src/model/GameStateBuilder.java
+++ RiskGameProject/src/model/GameStateBuilder.java
@@ -6,12 +6,14 @@ package model;
 public abstract class GameStateBuilder {
 	/**
 	 * This variable contains game state
+	 * @author Sai Krishna
 	 */
 	protected GameState gameState;
 	
 	/**
 	 * This function get the game state
 	 * @return This returns the game state
+	 * @author Sai Krishna
 	 */
 	public GameState getGameState() {
 		return gameState;
@@ -20,6 +22,7 @@ public abstract class GameStateBuilder {
 	/**
 	 * This function set the game state
 	 * @param gameState
+	 * @author Gagan Jaswal
 	 */
 	public void setGameState(GameState gameState) {
 		this.gameState = gameState;
@@ -28,18 +31,21 @@ public abstract class GameStateBuilder {
 	/**
 	 * This is an abstract method for setting the game map
 	 * @param gm
+	 * @author sakib
 	 */
 	public abstract void buildGameMap(GameMap gm);	
 	
 	/**
 	 * This is an abstract method for player list
 	 * @param pl
+	 * @author Ashish
 	 */
 	public abstract void buildPlayersList(PlayersList pl);
 	
 	/**
 	 * This is an abstract method for player
 	 * @param p
+	 * @author Gagan Jaswal
 	 */
 	public abstract void buildPlayer(Player p);	
 }
