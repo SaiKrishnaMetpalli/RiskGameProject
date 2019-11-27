6e7b46c69a3fcc5ee51c0e3683997671b91ef844
garima <garimadawar28@gmail.com>
2019-11-26 19:49:41 -0500

    javadoc added

--- RiskGameProject/src/controller/StrategyController.java
+++ RiskGameProject/src/controller/StrategyController.java
@@ -7,16 +7,31 @@ import model.GameMap;
 import model.Player;
 import model.PlayersList;
 
+/**
+ * This class contains all the methods for set Strategy and execute behaviour of Strategy
+ * 
+ * @author garimadawar
+ *
+ */
 public class StrategyController{
 	
+	/**
+	 * strategy this variables contains the strategy which is to be executed
+	 */
 	private Strategy strategy;
 	
-	
+	/**
+	 * This method is used to set the strategy
+	 * @param strategy
+	 */
 	public void setStrategy(Strategy strategy)
 	{
 		this.strategy = strategy;
 	}
-	
+	/**
+	 * This method is used to execute the strategy
+	 * @param strategy
+	 */
 	public String executeBehaviour(GameMap gm , PlayersList pl, Player player )
 	{	
 		return this.strategy.executeStrategy(gm, pl, player);		
