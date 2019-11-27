83b90426c3b287b7a849f46c559a66a501f3d353
Gagan Jaswal <gagandeepjaswal227@gmail.com>
2019-11-27 00:42:00 -0500

    Java doc for player list model

--- RiskGameProject/src/model/PlayersList.java
+++ RiskGameProject/src/model/PlayersList.java
@@ -2,22 +2,43 @@ package model;
 
 import java.util.HashMap;
 
+/**
+ * This class contains the player list
+ */
 public class PlayersList extends Observable {
-
+	
+	/**
+	 * This method contains the hashmap of list of players
+	 */
 	private HashMap<String, Player> listOfPlayers;
-
+	
+	/**
+	 * This function contains the player list
+	 */
 	public PlayersList() {
 		listOfPlayers = new HashMap<String, Player>();
 	}
-
+	
+	/**
+	 * This function get the list of players
+	 * @return This returns the list of players
+	 */
 	public HashMap<String, Player> getListOfPlayers() {
 		return listOfPlayers;
 	}
-
+	
+	/**
+	 * This function set the list of players
+	 * @param listOfPlayers  it contains the list of players
+	 */
 	public void setListOfPlayers(HashMap<String, Player> listOfPlayers) {
 		this.listOfPlayers = listOfPlayers;
 	}
-
+	
+	/**
+	 * This function notify the observer
+	 * @param p  it represents the player
+	 */
 	public void notifyToObserver(Player p) {
 		notifyObservers(this, p);
 	}
